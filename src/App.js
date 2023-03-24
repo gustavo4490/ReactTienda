75d99cca653509a4a27f5eb08852a76add016839
Andres <andresf150@gmail.com>
2023-03-24 14:43:50 -0600

    estructura del pryecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './assetss/css/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'bootstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br/>
+      <button className='btn btn-primary'>Hola mundo</button>
     </div>
   );
 }

1debbd13f739055c57ace23ee9fe48adcb6b2b3d
Andres <andresf150@gmail.com>
2023-03-24 16:22:53 -0600

    creando login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import '../assetss/css/login.css';
+import logo from '../assetss/img/iconoReact.png';
 
 class Login extends React.Component{
 
     render(){
         return(
-            <div>
-                Hola desde login
-            </div>
+           <React.Fragment>
+            <div className="wrapper fadeInDown">
+  <div id="formContent">
+
+    <div className="fadeIn first">
+      <img src={logo} width="100px" alt="User Icon" />
+    </div>
+
+    <form>
+      <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
+      <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
+      <input type="submit" className="fadeIn fourth" value="Log In" />
+    </form>
+
+    <div id="formFooter">
+      <a className="underlineHover" href="#">Forgot Password?</a>
+    </div>
+
+  </div>
+</div>
+           </React.Fragment>
         );
     }
 
---
+++ src/services/apiRest.js
@@ -0,0 +1,1 @@
+export const apiUrl = "https://productos.huehuetenango.com.gt/";

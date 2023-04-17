d2148c85829d2657fcfcae337e545d7e6f31a42e
Andres <andresf150@gmail.com>
2023-04-17 13:49:06 -0500

    nuevas rutas

--- src/App.js
+++ src/App.js
@@ -11,21 +11,16 @@ import Editar from './components/Editar';
 
 function App() {
   return (
-   <React.Fragment>
-      <Router>
-        <Switch>
-          <Route path='/' exact render={ props=> (<Login {...props}/>)}>
-          </Route>
-          <Route path='/dashboard' exact render={ props=> (<Dashboard {...props}/>)}>
-          </Route>
-          <Route path='/nuevo' exact render={ props=> (<Nuevo {...props}/>)}>
-          </Route>
-          <Route path='/editar' exact render={ props=> (<Editar {...props}/>)}>
-          </Route>
-        </Switch>
-      </Router>
-   </React.Fragment>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/nuevo" component={Nuevo} />
+        <Route exact path="/editar" component={Editar} />
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
